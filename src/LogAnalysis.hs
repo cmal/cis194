960443705ab33f4a54b9e50f47173aa8da44827a
Yu Zhao <yu.zhao10@ele.me>
2019-12-06 15:38:11 +0800

    feat: week02 LogAnalysis

---
+++ src/LogAnalysis.hs
@@ -0,0 +1,48 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+
+import Log
+
+parseMessage :: String -> LogMessage
+parseMessage s =
+  case t of
+    "E" -> LogMessage (Error l) (read $ head $ drop 2 msgs :: Int) (unwords $ drop 3 msgs)
+    "W" -> LogMessage Warning l (unwords $ drop 2 msgs)
+    "I" -> LogMessage Info l (unwords $ drop 2 msgs)
+    _ -> Unknown s
+  where msgs = words s
+        t = head msgs
+        l = read $ head $ tail msgs :: Int
+        
+parse :: String -> [LogMessage]
+parse s =  map parseMessage $ lines s
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) t = t
+insert m Leaf = Node Leaf m Leaf
+insert m@(LogMessage _ ts _) (Node lt r@(LogMessage _ tsr _) rt)
+  | ts <= tsr = Node (insert m lt) r rt
+  | ts > tsr = Node lt r (insert m rt)
+
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node lt m rt) = (inOrder lt) ++ [m] ++ (inOrder rt)
+
+
+isCriticalError :: LogMessage -> Bool
+isCriticalError (LogMessage (Error l) _ _) = l >= 50
+isCriticalError _ = False
+
+toString :: LogMessage -> String
+toString (LogMessage _ _ s) = s
+toString (Unknown s) = s
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = map toString . filter isCriticalError
+
+-- testParse parse 10 "./error.log"
+-- testWhatWentWrong parse whatWentWrong "error.log"
---
+++ test/W02LogAnalysisSpec.hs
@@ -0,0 +1,38 @@
+module W02LogAnalysisSpec (spec) where
+
+import Test.Hspec
+import Log
+import LogAnalysis
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "parseMessage" $ do
+    it "parseMessage" $ do
+      parseMessage "E 2 562 help help" `shouldBe` LogMessage (Error 2) 562 "help help"
+
+  describe "parse" $ do
+    it "testParse error.log" $ do
+      testParse parse 2 "src/error.log" `shouldReturn`
+        [ LogMessage Info 5053 "pci_id: con ing!"
+        , LogMessage Info 4681 "ehci 0xf43d000:15: regista14: [0xbffff 0xfed nosabled 00-02] Zonseres: brips byted nored)"
+        ]
+    it "testParse sample.log" $ do
+      testParse parse 2 "src/sample.log" `shouldReturn`
+        [ LogMessage Info 6 "Completed armadillo processing"
+        , LogMessage Info 1 "Nothing to report"
+        ]
+      
+  describe "whatWentWrong" $ do
+    it "testWhatWentWrong error.log" $ do
+      testWhatWentWrong parse whatWentWrong "src/error.log" `shouldReturn` []
+    it "testWhatWentWrong sample" $ do
+      testWhatWentWrong parse whatWentWrong "src/sample.log" `shouldReturn` 
+        [ "Way too many pickles"
+        , "Bad pickle-flange interaction detected"
+        , "Flange failed!"
+        ]
+      
+
