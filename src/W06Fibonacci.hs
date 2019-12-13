d49b10702a1825bf6fe8534e8af69946c3eeff98
Yu Zhao <yu.zhao10@ele.me>
2019-12-13 16:24:33 +0800

    feat: week 06 fibonacci

--- src/W06Fibonacci.hs
+++ src/W06Fibonacci.hs
@@ -14,7 +14,7 @@ fibs1 = map fib [0..]
 
 fibs2 :: [Integer]
 fibs2 = 1:1:(go 2)
-            where go n = (fibs2!!(pred n) Prelude.+ fibs2!!(pred $ pred n)):(go (succ n))
+            where go n = (fibs2!!(pred n) + fibs2!!(pred $ pred n)):(go (succ n))
 
 
 data Stream a = Cons a (Stream a)
@@ -62,3 +62,31 @@ instance Num (Stream Integer) where
   negate = streamMap negate
   (+) (Cons x1 s1) (Cons x2 s2) = Cons (x1 + x2) (s1 + s2) 
   (*) (Cons x1 r1) s2@(Cons x2 r2) = Cons (x1 * x2) $ (streamMap (* x1) r2) + r1 * s2
+
+instance Fractional (Stream Integer) where
+  (/) (Cons x1 r1) (Cons x2 r2) = q 
+       where q = Cons (div x1 x2) $ streamMap ((div 1 x2) *) (r1 + streamMap negate q * r2)
+
+
+-- 1 - x - x^2
+
+fib3 :: Stream Integer
+fib3 = x / (Cons 1 $ Cons (-1) $ Cons (-1) $ streamRepeat 0)
+  
+fib3_ :: [Integer]
+fib3_ = tail $ streamToList fib3
+
+-- Q7
+
+data Matrix = Matrix Integer Integer Integer Integer  -- a11, a12, a21, a22
+instance Num Matrix where
+  fromInteger n = Matrix n 0 0 0
+  negate (Matrix a b c d) = Matrix (negate a) (negate b) (negate c) (negate b)
+  (+) (Matrix a1 b1 c1 d1) (Matrix a2 b2 c2 d2) = Matrix (a1 + a2) (b1 + b2) (c1 + c2) (d1 + d2)
+  (*) (Matrix a1 b1 c1 d1) (Matrix a2 b2 c2 d2) = Matrix (a1 * a2 + b1 * c2) (a1 * b2 + b1 * d2) (c1 * a2 + d1 * c2) (c1 * b2 + d1 * d2)
+
+f = Matrix 1 1 1 0
+
+fib4 :: Integer -> Integer
+fib4 n = x 
+  where Matrix x _ _ _ = f ^ n
--- test/W06FibonacciSpec.hs
+++ test/W06FibonacciSpec.hs
@@ -8,5 +8,29 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "fib1" $ do
-    it "fib1" $ do
+  describe "fib" $ do
+    it "fib" $ do
+       fib 0 `shouldBe` 1
+    it "fib" $ do
+       fib 10 `shouldBe` 89
+
+  describe "fibs1" $ do
+    it "fibs1" $ do
+       take 10 fibs1 `shouldBe` [1,1,2,3,5,8,13,21,34,55]
+
+  describe "fibs2" $ do
+    it "fibs2" $ do
+      take 10 fibs2 `shouldBe` [1,1,2,3,5,8,13,21,34,55]
+
+
+  describe "fib3" $ do
+    it "fib3" $ do
+      show fib3 `shouldBe` "0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,..."
+    it "fib3_" $ do
+      take 10 fib3_ `shouldBe` [1,1,2,3,5,8,13,21,34,55]
+
+  describe "fib4" $ do
+    it "fib4 0" $ do
+      fib4 0 `shouldBe` 1
+    it "fib4 10" $ do
+      fib4 10 `shouldBe` 89
